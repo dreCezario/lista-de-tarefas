8cef47d07431b8214c5113a74f401ee632c450c8
André <developer.andrecezario@gmail.com>
2023-01-09 12:56:59 -0300

    adicionando a versão da tarefa concluída

--- js/main.js
+++ js/main.js
@@ -7,21 +7,29 @@ const Main = {
     },/*Função responsável por iniciar as funções*/
 
     cacheSelectors: function() {
-        this.checkButtons = document.querySelectorAll('.check')
+        this.$checkButtons = document.querySelectorAll('.check')
     }, /*Função responsável por selecionar os elementos do html e armazena-los numa variável */
 
     bindEvents: function() {
         const self = this
 
-        this.checkButtons.forEach(function(button) {
+        this.$checkButtons.forEach(function(button) {
             button.onclick = self.Events.checkButton_click
         })
     }, /*Função responsável por adicionar eventos nos elementos html */
 
 
     Events: {
-        checkButton_click: function() {
-            alert('ok')
+        checkButton_click: function(e) {
+            const li = (e.target.parentElement)
+            const isDone = li.classList.contains('done')
+
+            if(!isDone){
+                return li.classList.add('done')
+            }
+                li.classList.remove('done')
+            
+
         }
     }
 }
